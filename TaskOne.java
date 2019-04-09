074aa6c1fd5834177dc18da9aa17ec4db9b0d457
18SW42 <49362003+18SW42@users.noreply.github.com>
2019-04-09 10:46:55 +0500

    Add files via upload

---
+++ TaskOne.java
@@ -0,0 +1,21 @@
+class TaskOne{
+public static void main(String args[])
+	{
+		String name = "Sundar Kumar";
+		String semester = "1st";
+		double totalmarks = 60;
+		char grade = 'C';
+		float english = 17.5;
+		short math = 16;
+		short cp = 16;
+
+		System.out.println("\n Name : " + name);
+		System.out.println("\n Semester : " + semester);
+		System.out.println("\n Total Marks = " + totalmarks);
+		System.out.println("\n Marks in English = " + english);
+		System.out.println("\n Marks in Appplied Calculus = " + math);
+		System.out.println("\n Marks in Computer Programmming = " + cp);
+		System.out.println("\n Grade : " + grade);
+	}
+}
+
---
+++ TaskTwo.java
@@ -0,0 +1,13 @@
+class TaskTwo
+{
+	public static void main(String args[])
+	{
+		double v1 = 50.25;
+		long intpt = (long)v1;
+		double fractpt = (v1 - intpt);
+
+		System.out.println(" Exact Value : " + v1);
+		System.out.println(" Integer Part : " + intpt);
+		System.out.println(" Fractional Part : " + fractpt);
+	}
+}
